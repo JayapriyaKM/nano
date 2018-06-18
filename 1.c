5a57a32ef6ff2acf22c1ee9c3e898a2f2576ea77
VijayaGopika <vijaya.gopika@vvdntech.com>
2018-06-18 15:06:11 +0530

    1.c

--- 1.c
+++ 1.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-
-iknt add(int, int);
-int main()
-{
-    int a =1;
-    int b= 10;
-    add(a,b);
-}
-dncjdncdjfje
-int add (a, b)
-{
-    int c = 0;
-    c=a+b+1;
-    printf("%d\n",c);
-    int gxrx = 0;
-   return 0;
-}
+jjjjjj
