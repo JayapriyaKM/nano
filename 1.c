6528bb68ed258d65c8252138e0995147752c8177
JayapriyaKM <jayapriya.km@vvdntech.com>
2018-06-18 13:12:13 +0530

    1.c

--- 1.c
+++ 1.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int add(int, int);
+iknt add(int, int);
 int main()
 {
     int a =1;
     int b= 10;
     add(a,b);
 }
-
+dncjdncdjfje
 int add (a, b)
 {
     int c = 0;
-    c=a+b;
+    c=a+b+1;
     printf("%d\n",c);
     int gxrx = 0;
    return 0;
